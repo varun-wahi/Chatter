2695322bbf0fbb9587d606d08313b3614502798b
Varun <varun@wandermewell.com>
2024-11-21 14:05:05 +0530

    update: backend complete

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 
 import cloudinary from "../lib/cloudinary.js";
-import { getReceiverSocketId, io } from "../lib/socket.js";
+// import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -57,10 +57,12 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    const receiverSocketId = getReceiverSocketId(receiverId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
-    }
+    //socket.io  implementation for realtime messaging
+    //
+    // const receiverSocketId = getReceiverSocketId(receiverId);
+    // if (receiverSocketId) {
+    //   io.to(receiverSocketId).emit("newMessage", newMessage);
+    // }
 
     res.status(201).json(newMessage);
   } catch (error) {
